0dcb243bc605dbeee62e4de8ffb214b9fb659c45
samsim97 <samuel.simard97@hotmail.com>
2024-11-04 20:39:14 -0500

    Revert "git cache cleared"
    This reverts commit d8398f02375b99fbc88671abd36f0752ead4759c.

--- src/gui/ControlsWindow.cpp
+++
@@ -1,9 +0,0 @@
-#include "ControlsWindow.h"
-
-#include <imgui.h>
-
-void ControlsWindow::draw() {
-    ImGui::Begin("Controls");
-    ImGui::Button("Launch la fusée");
-    ImGui::End();
-}
--- src/gui/ControlsWindow.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONTROLSWINDOW_H
-#define CONTROLSWINDOW_H
-
-class ControlsWindow {
-public:
-    void draw();
-};
-
-#endif //CONTROLSWINDOW_H
